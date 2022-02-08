ced9c4a996144f96d21bd980b6d5ba4261626055
Renan Freire <renangomesfreire@gmail.com>
2022-02-08 14:29:37 -0300

    add in github network

---
+++ public/scripts/main.js
@@ -0,0 +1,46 @@
+import Modal from './modal.js';
+
+// MODAL
+const modal = Modal()
+
+
+// PEGAR TODOS OS BOTOÕES QUE EXISTE COM A CLASSE CHECK
+const checkButtons = document.querySelectorAll(".actions a.check")
+const deleteButtons = document.querySelectorAll(".actions a.delete")
+
+// Alteração para modal
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
+
+function handleClick(event, check = true) {
+    event.preventDefault()
+
+    const text = check ? "Marcar como lida" : "Excluir";
+    const slug = check? "check" : "delete"
+
+    const roomId = document.querySelector('#room-id').dataset.id
+
+    const questionId = event.target.dataset.id
+
+    const form = document.querySelector(".modal form")
+    form.setAttribute("action", `/room/${roomId}/${questionId}/${slug}`)
+
+    modalTitle.innerHTML = `${text} esta pergunta?`
+    modalDescription.innerHTML =  `Tem certeza que deseja ${text.toLowerCase()} esta pergunta`
+    modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
+    check? modalButton.classList.remove("red") : modalButton.classList.add("red")
+
+    modal.open()
+}
+
+// PEGAR QUANDO O MARCAR COMO LIDO QUANdO CLICADO
+checkButtons.forEach(button => {
+    //add eventlistner
+    button.addEventListener("click", handleClick)
+})
+
+deleteButtons.forEach(button => {
+    button.addEventListener("click", (event) => handleClick(event, false))
+})
---
+++ src/route.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const QuestionController = require('./controllers/QuestionController')
+
+const route = express.Router()
+
+route.get('/', (req, res) => {
+    res.render("index")
+})
+route.get('/room', (req, res) => {
+    res.render("room")
+})
+route.get('/create-pass', (req, res) => {
+    res.render("create-pass")
+})
+
+// Formato que o formulario de dentro da modal tem que passar a informação
+route.post('/room/:room/:question/:action', QuestionController.index)
+
+module.exports = route
